d29fa0ac6f2c67f0474b88d0c1f0f78c2c135a95
Priyanshu Maikhuri <maikhuripriyanshu@gmail.com>
2023-04-22 22:29:30 +0530

    Second Commit - To add app icon

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,15 +1,23 @@
 import 'package:flutter/material.dart';
+import '../presentation/resources/theme_manager.dart';
 
 class MyApp extends StatefulWidget {
-  const MyApp({super.key});
+  MyApp._internal(); // private named constructor
+  int appState = 0;
+  static final MyApp instance =
+      MyApp._internal(); // single instance -- singleton
+
+  factory MyApp() => instance; // factory for the class instance
 
   @override
-  State<MyApp> createState() => _MyAppState();
+  _MyAppState createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      theme: getApplicationTheme(),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './app/app.dart';
+
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
