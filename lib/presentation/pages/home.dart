7eb0db164e6cfea486b540806e80aa144b865648
Rahul Shah <iq.nit.student@gmail.com>
2023-05-14 12:35:56 +0530

    serch bar update

--- lib/presentation/pages/home.dart
+++ lib/presentation/pages/home.dart
@@ -34,6 +34,7 @@ class _HomeState extends State<Home> {
               GFLoader(type: GFLoaderType.circle),
               GoogleMap(
                 mapType: MapType.normal,
+                
                 initialCameraPosition: _kGooglePlex,
                 onMapCreated: (GoogleMapController controller) {
                   _controller.complete(controller);
@@ -41,59 +42,49 @@ class _HomeState extends State<Home> {
                 zoomControlsEnabled: false,
                 compassEnabled: false,
               ),
-              Positioned(
-                  top: 20,
-                  left: 0.01,
-                  right: 12,
-                  child: GFSearchBar(
-                    textColor: Colors.white70,
-                    searchList: list,
-                    searchBoxInputDecoration: InputDecoration(
-                      filled: true,
-                      fillColor: ColorManager.appBlack,
-                      labelText: 'Search here...',
-                      icon: Container(
-                        width: 42,
-                        height: 42,
-                        child: GFBorder(
-                          padding: EdgeInsets.all(1),
-                          strokeWidth: 3,
-                          color: ColorManager.primary,
-                          dashedLine: [2, 0],
-                          type: GFBorderType.circle,
-                          radius: Radius.circular(100),
-                          child: Image.asset(
-                            'assets/images/lightning_icon.png',
-                            height: 35,
-                            color: ColorManager.primary,
-                            fit: BoxFit.contain,
-                          ),
-                        ),
+              Padding(
+                padding: EdgeInsets.only(
+                    top: MediaQuery.of(context).size.height * 0.1),
+                child: Positioned(
+                    top: 20,
+                    left: 0.01,
+                    right: 12,
+                    child: GFSearchBar(
+                      textColor: Colors.white70,
+                      searchList: list,
+                      
+                      searchBoxInputDecoration: InputDecoration(
+                        filled: true,
+                        prefixIcon: Icon(Icons.bolt),
+                        prefixIconColor: ColorManager.primary,
+                        
+                        fillColor: ColorManager.appBlack,
+                        labelText: 'Search here...',
                       ),
-                    ),
-                    searchQueryBuilder: (query, list) {
-                      return list
-                          .where((item) => item
-                              .toString()
-                              .toLowerCase()
-                              .contains(query.toLowerCase()))
-                          .toList();
-                    },
-                    overlaySearchListItemBuilder: (item) {
-                      return Container(
-                        margin: EdgeInsets.only(top: 30),
-                        //padding: const EdgeInsets.all(8),
-                        //height: (MediaQuery.of(context).size.height - MediaQuery.of(context).padding.top)*0,
-                        child: Text(
-                          "${item}",
-                          style: const TextStyle(fontSize: 18),
-                        ),
-                      );
-                    },
-                    onItemSelected: (item) {
-                      setState(() => print('$item'));
-                    },
-                  )),
+                      searchQueryBuilder: (query, list) {
+                        return list
+                            .where((item) => item
+                                .toString()
+                                .toLowerCase()
+                                .contains(query.toLowerCase()))
+                            .toList();
+                      },
+                      overlaySearchListItemBuilder: (item) {
+                        return Container(
+                          margin: EdgeInsets.only(top: 30),
+                          //padding: const EdgeInsets.all(8),
+                          //height: (MediaQuery.of(context).size.height - MediaQuery.of(context).padding.top)*0,
+                          child: Text(
+                            "${item}",
+                            style: const TextStyle(fontSize: 18),
+                          ),
+                        );
+                      },
+                      onItemSelected: (item) {
+                        setState(() => print('$item'));
+                      },
+                    )),
+              ),
             ],
           ),
         ));
