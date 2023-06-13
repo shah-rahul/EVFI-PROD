a6b5bcf82c79c3ca09c7a1d700f2d9de243730fa
Rahul Shah <iq.nit.student@gmail.com>
2023-06-14 04:42:21 +0530

    simple search

--- lib/presentation/pages/homePage/search/search_page.dart
+++ lib/presentation/pages/homePage/search/search_page.dart
@@ -1,46 +1,130 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:EVFI/presentation/pages/homePage/search/data_type.dart';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:http/http.dart' as http;
 
-import '../../../resources/assets_manager.dart';
-import './toFrom.dart';
 
-class SearchPage extends StatelessWidget {
+class SearchPage extends StatefulWidget {
   const SearchPage({Key? key}) : super(key: key);
 
   @override
+  State<SearchPage> createState() => _SearchPageState();
+}
+
+class _SearchPageState extends State<SearchPage> {
+  final TextEditingController _sourceController = TextEditingController();
+  final TextEditingController _destinationController = TextEditingController();
+  Timer? _debounce;
+  List<OSMdata> _options = <OSMdata>[];
+  @override
   Widget build(BuildContext context) {
+    Widget _buildListView() {
+      return ListView.builder(
+          shrinkWrap: true,
+          physics: const NeverScrollableScrollPhysics(),
+          itemCount: _options.length > 5 ? 5 : _options.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              leading: Icon(Icons.location_on, color: Colors.black),
+              title: Text(
+                _options[index].displayname,
+                style: TextStyle(color: Colors.black),
+              ),
+              onTap: () {
+                print(
+                 
+                  LatLng(_options[index].latitude, _options[index].longitude),
+                );
+                print(_options[index].displayname);
+                print( _options[index].displayname,);
+
+                _options.clear();
+                setState(() {});
+              },
+            );
+          });
+    }
+
+    Widget _buildSearchBar(TextEditingController controller) {
+      OutlineInputBorder inputBorder = OutlineInputBorder(
+        borderSide: BorderSide(color: Theme.of(context).primaryColor),
+      );
+      OutlineInputBorder inputFocusBorder = OutlineInputBorder(
+        borderSide:
+            BorderSide(color: Theme.of(context).primaryColor, width: 3.0),
+      );
+
+      return Column(
+        children: [
+          TextFormField(
+              style: TextStyle(color: Colors.black),
+              controller: controller,
+              decoration: InputDecoration(
+                hintText: "Search",
+                border: inputBorder,
+                focusedBorder: inputFocusBorder,
+                suffixIcon: IconButton(
+                  onPressed: () => controller.clear(),
+                  icon: Icon(
+                    Icons.clear,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+              onChanged: (String value) {
+                if (_debounce?.isActive ?? false) {
+                  _debounce?.cancel();
+                }
+                setState(() {});
+                _debounce = Timer(const Duration(milliseconds: 20), () async {
+                  var client = http.Client();
+                  try {
+                    String url =
+                        'https://nominatim.openstreetmap.org/search?q=$value&format=json&polygon_geojson=1&addressdetails=1&accept-language=en';
+                    var response = await client.post(Uri.parse(url));
+                    var decodedResponse =
+                        jsonDecode(utf8.decode(response.bodyBytes))
+                            as List<dynamic>;
+                    _options = decodedResponse
+                        .map((e) => OSMdata(
+                            displayname: e['display_name'],
+                            latitude: double.parse(e['lat']),
+                            longitude: double.parse(e['lon'])))
+                        .toList();
+                    setState(() {});
+                  } on Exception catch (e) {
+                    print(e);
+                  } finally {
+                    client.close();
+                  }
+                });
+              }),
+          StatefulBuilder(builder: ((context, setState) {
+            return _buildListView();
+          })),
+        ],
+      );
+    }
+
+    final _startPointController = TextEditingController();
     final mq = MediaQuery.of(context);
     return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
+          child: Container(
+        height: mq.size.height * 0.5,
+        width: mq.size.width,
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            SizedBox(
-              height: mq.size.height * 0.25,
-              width: double.infinity,
-              child: Card(
-                elevation: 2,
-                child: Container(
-                  padding: const EdgeInsets.all(0),
-                  child: const ToFrom(),
-                ),
-              ),
-            ),
-            const Expanded(
-                child: Center(
-                    child: Image(
-                        image: AssetImage(ImageAssets.onboardingLogo2)))),
-            const Expanded(
-                child: Center(
-              child: Text(
-                "Search Now",
-                style: TextStyle(fontSize: 25),
-              ),
-            )),
+            _buildSearchBar(_sourceController),
+            // _buildSearchBar(_destinationController),
+
           ],
         ),
-      ),
+      )),
     );
   }
 }
