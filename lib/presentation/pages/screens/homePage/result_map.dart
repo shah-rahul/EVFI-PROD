a30667e662cd3993fb1dba50e6bddebe8964bc77
Priyanshu Maikhuri <maikhuripriyanshu@gmail.com>
2023-06-17 20:32:29 +0530

    Places Autocomplete Done

--- lib/presentation/pages/screens/homePage/result_map.dart
+++ lib/presentation/pages/screens/homePage/result_map.dart
@@ -1,15 +1,19 @@
 import 'dart:async';
+import 'dart:convert';
 import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:getwidget/getwidget.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:http/http.dart' as http;
 
+import '../../models/data_type.dart';
 import '../../../resources/color_manager.dart';
 
 class ResultMap extends StatefulWidget {
-  final List<LatLng> routeSet;
-  const ResultMap({required this.routeSet});
+  final LatLong startL, endL;
+  const ResultMap({Key? key, required this.startL, required this.endL})
+      : super(key: key);
 
   @override
   State<ResultMap> createState() => _ResultMapState();
@@ -19,12 +23,64 @@ class _ResultMapState extends State<ResultMap> {
   final Completer<GoogleMapController> _controller =
       Completer<GoogleMapController>();
 
+  List<LatLng> routpoints = [LatLng(28.628117, 77.173563)];
   Set<Polyline> polylines = {};
   LatLng? initialCoordinate, finalCoordinate;
-  Marker? sourceMarker, destMarker;
+  //late Marker sourceMarker, destMarker;
   late LatLngBounds bounds;
+  bool _isLoading = false;
+
+  // void setMarkers(List<LatLng> routpoints) {
+  //   setState(() {
+  //     if (routpoints.isNotEmpty) {
+  //       print('Calculate Markers');
+  //       sourceMarker = Marker(
+  //         markerId: const MarkerId('sourceMarker'),
+  //         position: routpoints.first,
+  //         icon:
+  //             BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueAzure),
+  //       );
+  //       destMarker = Marker(
+  //         markerId: const MarkerId('_destMarker'),
+  //         position: routpoints.last,
+  //         icon: BitmapDescriptor.defaultMarker,
+  //       );
+  //     }
+  //   });
+  // }
+
+  void _calculateRoute(var v1, var v2, var v3, var v4) async {
+    try {
+      var url = Uri.parse(
+          'http://router.project-osrm.org/route/v1/driving/$v2,$v1;$v4,$v3?steps=true&annotations=true&geometries=geojson&overview=full');
+      var response = await http.get(url);
+
+      setState(() {
+        routpoints = [];
+        _isLoading = true;
+        var ruter =
+            jsonDecode(response.body)['routes'][0]['geometry']['coordinates'];
+        for (int i = 0; i < ruter.length; i++) {
+          var reep = ruter[i].toString();
+          reep = reep.replaceAll("[", "");
+          reep = reep.replaceAll("]", "");
+          var lat1 = reep.split(',');
+          var long1 = reep.split(",");
+          routpoints.add(LatLng(double.parse(lat1[1]), double.parse(long1[0])));
+        }
+        //setMarkers(routpoints);
+      });
+    } on Exception catch (e) {
+      print(e);
+      print('Exception found!');
+    }
+    updatePolyline(routpoints);
+  }
 
   void updatePolyline(List<LatLng> polylineCoordinates) {
+    print('See Here');
+    print(polylineCoordinates);
+
     PolylineId polylineId = const PolylineId('polyline_1');
     Polyline polyline = Polyline(
       polylineId: polylineId,
@@ -52,78 +108,87 @@ class _ResultMapState extends State<ResultMap> {
         northeast: LatLng(maxLat, maxLng),
         southwest: LatLng(minLat, minLng),
       );
+      _isLoading = false;
     });
   }
 
   @override
   void initState() {
     super.initState();
-    updatePolyline(widget.routeSet);
+    var v1 = widget.startL.latitude;
+    var v2 = widget.startL.longitude;
+    var v3 = widget.endL.latitude;
+    var v4 = widget.endL.longitude;
+    _calculateRoute(v1, v2, v3, v4);
+  }
 
-    if (widget.routeSet.isNotEmpty) {
-      initialCoordinate = widget
-          .routeSet[0]; // Set initial coordinate of camera view for polyline
-      sourceMarker = Marker(
-        markerId: const MarkerId('sourceMarker'),
-        position: initialCoordinate!,
-        icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueAzure),
-      );
-      finalCoordinate = widget.routeSet.last;
-      destMarker = Marker(
-        markerId: const MarkerId('_destMarker'),
-        position: finalCoordinate!,
-        icon: BitmapDescriptor.defaultMarker,
-      );
-    }
+  Widget _buildBackKey() {
+    return Positioned(
+      top: MediaQuery.of(context).size.height * 0.05,
+      left: 15,
+      child: Container(
+          decoration: BoxDecoration(
+            border: Border.all(
+              color: ColorManager.primary,
+              width: 1.5,
+            ),
+            borderRadius: BorderRadius.circular(100),
+          ),
+          child: IconButton(
+            icon: Icon(
+              Icons.arrow_back_ios_outlined,
+              color: ColorManager.appBlack,
+              size: 23.0,
+            ),
+            onPressed: () => Navigator.of(context).pop(),
+          )),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: ColorManager.lightGrey,
-        body: SizedBox(
-            height: MediaQuery.of(context).size.height,
-            child: Stack(children: [
-              const GFLoader(type: GFLoaderType.circle),
-              GoogleMap(
-                onMapCreated: (GoogleMapController controller) {
-                  _controller.complete(controller);
-                  setState(() {
-                    controller
-                      .animateCamera(CameraUpdate.newLatLngBounds(bounds, 60));
-                  });
-                },
-                mapType: MapType.normal,
-                markers: {sourceMarker!, destMarker!},
-                initialCameraPosition: const CameraPosition(
-                  target: LatLng(28.6001740, 77.2105709),
-                  zoom: 13.537, //14.4746,
-                ),
-                polylines: polylines,
-                zoomControlsEnabled: true,
-                compassEnabled: false,
-                myLocationEnabled: true,
-              ),
-              Positioned(
-                top: MediaQuery.of(context).size.height * 0.05,
-                left: 15,
-                child: Container(
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: ColorManager.primary,
-                        width: 1.5,
-                      ),
-                      borderRadius: BorderRadius.circular(100),
+        body: Visibility(
+          visible: _isLoading,
+          child: const GFLoader(type: GFLoaderType.circle),
+          replacement: SizedBox(
+              height: MediaQuery.of(context).size.height,
+              child: Stack(children: [
+                const GFLoader(type: GFLoaderType.circle),
+                GoogleMap(
+                  onMapCreated: (GoogleMapController controller) {
+                    _controller.complete(controller);
+                    setState(() {
+                      controller.animateCamera(
+                          CameraUpdate.newLatLngBounds(bounds, 60));
+                    });
+                  },
+                  mapType: MapType.normal,
+                  markers: {
+                    Marker(
+                      markerId: const MarkerId('sourceMarker'),
+                      position: routpoints.first,
+                      icon: BitmapDescriptor.defaultMarkerWithHue(
+                          BitmapDescriptor.hueAzure),
                     ),
-                    child: IconButton(
-                      icon: Icon(
-                        Icons.arrow_back_ios_outlined,
-                        color: ColorManager.appBlack,
-                        size: 23.0,
-                      ),
-                      onPressed: () => Navigator.of(context).pop(),
-                    )),
-              )
-            ])));
+                    Marker(
+                      markerId: const MarkerId('_destMarker'),
+                      position: routpoints.last,
+                      icon: BitmapDescriptor.defaultMarker,
+                    )
+                  }, //{sourceMarker, destMarker},
+                  initialCameraPosition: const CameraPosition(
+                    target: LatLng(28.6001740, 77.2105709),
+                    zoom: 13.437, //14.4746,
+                  ),
+                  polylines: polylines,
+                  zoomControlsEnabled: true,
+                  compassEnabled: false,
+                  myLocationButtonEnabled: false,
+                ),
+                _buildBackKey(),
+              ])),
+        ));
   }
 }
--- lib/presentation/pages/screens/homePage/search_page.dart
+++ lib/presentation/pages/screens/homePage/search_page.dart
@@ -1,43 +1,209 @@
+import 'dart:async';
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:http/http.dart' as http;
 
-import '../../../resources/assets_manager.dart';
-import '../../widgets/to_from.dart';
+import '/presentation/resources/color_manager.dart';
+import '../../models/data_type.dart';
+import './result_map.dart';
 
-class SearchPage extends StatelessWidget {
+class SearchPage extends StatefulWidget {
   const SearchPage({Key? key}) : super(key: key);
 
   @override
+  State<SearchPage> createState() => _SearchPageState();
+}
+
+class _SearchPageState extends State<SearchPage> {
+  final controller1 = TextEditingController();
+  final controller2 = TextEditingController();
+  Timer? _debounce;
+  List<OSMdata> _options = <OSMdata>[];
+  LatLong start = LatLong(-51.42, 95.47);  //random ocean coordinates
+  LatLong end = LatLong(-51.42, 95.47);
+
+get _checkIfDifferent {
+    if(start.latitude == end.latitude && start.longitude == end.longitude){
+      return false;
+    }
+    else {
+      return true;
+    }
+  }
+
+  Widget _buildListView(TextEditingController controller, bool val) {
+    return ListView.builder(
+      shrinkWrap: true,
+      physics: const NeverScrollableScrollPhysics(),
+      itemBuilder: (context, index) => Column(
+        children: [
+          ListTile(
+            leading: CircleAvatar(
+                backgroundColor: ColorManager.appBlack.withOpacity(0.16),
+                child: const Icon(
+                  Icons.location_on,
+                  color: Colors.black,
+                )),
+            title: Text(_options[index].displayname,
+                style: TextStyle(color: ColorManager.appBlack)),
+            trailing: const Icon(Icons.north_west, color: Colors.grey),
+            onTap: () {
+              print(_options[index].displayname);
+              print(
+                LatLng(_options[index].latitude, _options[index].longitude),
+              );
+              setState(() {
+                controller.text = _options[index].displayname;
+                (val)
+                    ? {
+                        start = LatLong(
+                            _options[index].latitude, _options[index].longitude)
+                      }
+                    : {
+                        end = LatLong(
+                            _options[index].latitude, _options[index].longitude)
+                      };
+              });
+              _options.clear();
+              if (controller1.text.isNotEmpty && controller2.text.isNotEmpty) {
+                if (_checkIfDifferent) {
+                  Navigator.of(context).pushReplacement(
+                    MaterialPageRoute(
+                      builder: (context) => ResultMap(
+                        startL: start, 
+                        endL: end
+                      ),
+                    ),
+                  );
+                }
+              }
+            },
+          ),
+          const Divider(
+            thickness: 1.50,
+          ),
+        ],
+      ),
+      itemCount: _options.length > 6 ? 6 : _options.length,
+    );
+  }
+
+  Widget _buildTextFormField(TextEditingController controller, String hint,
+      IconData icon, Color color) {
+    return TextFormField(
+        controller: controller,
+        style: TextStyle(color: ColorManager.appBlack),
+        keyboardType: TextInputType.streetAddress,
+        decoration: InputDecoration(
+          labelText: hint,
+          contentPadding: const EdgeInsets.all(10),
+          prefixIcon: Icon(
+            icon,
+            color: color,
+          ),
+          suffixIcon: controller.text.isEmpty
+              ? Container(width: 0)
+              : IconButton(
+                  onPressed: () => controller.clear(),
+                  icon: const Icon(
+                    Icons.close,
+                  ),
+                ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: ColorManager.primary, width: 3.0),
+          ),
+        ),
+        onChanged: (String value) {
+          if (_debounce?.isActive ?? false) {
+            _debounce?.cancel();
+          }
+          setState(() {});
+          _debounce = Timer(const Duration(milliseconds: 25), () async {
+            var client = http.Client();
+            try {
+              String url =
+                  'https://nominatim.openstreetmap.org/search?q=$value&format=json&polygon_geojson=1&addressdetails=1&accept-language=en';
+              var response = await client.post(Uri.parse(url));
+              var decodedResponse =
+                  jsonDecode(utf8.decode(response.bodyBytes)) as List<dynamic>;
+              _options = decodedResponse
+                  .map((e) => OSMdata(
+                      displayname: e['display_name'],
+                      latitude: double.parse(e['lat']),
+                      longitude: double.parse(e['lon'])))
+                  .toList();
+              setState(() {});
+            } on Exception catch (e) {
+              print(e);
+            } finally {
+              client.close();
+            }
+          });
+        });
+  }
+
+  void _swap(TextEditingController c1, TextEditingController c2) {
+    String tempController = c1.text;
+    c1.text = c2.text;
+    c2.text = tempController;
+    LatLong temp = start;
+    start = end;
+    end = temp;
+    setState(() {});
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final mq = MediaQuery.of(context);
     return Scaffold(
       backgroundColor: Colors.white,
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () => Navigator.of(context).pop(),
+          icon: const Icon(
+            Icons.arrow_back,
+            size: 30,
+          ),
+          color: ColorManager.appBlack,
+        ),
+        backgroundColor: Colors.white,
+        elevation: 0,
+      ),
       body: SafeArea(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            SizedBox(
-              height: mq.size.height * 0.25,
-              width: double.infinity,
-              child: Card(
-                elevation: 2,
-                child: Container(
-                  padding: const EdgeInsets.all(0),
-                  child: const ToFrom(),
-                ),
-              ),
+            Card(
+              margin: EdgeInsets.zero,
+              elevation: 3,
+              child: Container(
+                  padding: const EdgeInsets.only(
+                      bottom: 30, top: 10, left: 25, right: 25),
+                  width: double.infinity,
+                  child: Column(children: [
+                    _buildTextFormField(controller1, 'Start location',
+                        Icons.location_on_outlined, Colors.blue),
+                    IconButton(
+                      onPressed: () => _swap(controller1, controller2),
+                      icon: const Icon(
+                        Icons.swap_vert,
+                        size: 33,
+                      ),
+                      color: ColorManager.appBlack,
+                    ),
+                    _buildTextFormField(controller2, 'Destination location',
+                        Icons.location_on_outlined, Colors.red),
+                  ])),
             ),
-            const Expanded(
-                child: Center(
-                    child: Image(
-                        image: AssetImage(ImageAssets.onboardingLogo2)))),
-            const Expanded(
-                child: Center(
-              child: Text(
-                "Search Now",
-                style: TextStyle(fontSize: 25),
-              ),
-            )),
+            Expanded(
+              child: StatefulBuilder(builder: ((context, setState) {
+                if (controller1.selection.isValid) {
+                  return _buildListView(controller1, true);
+                } else {
+                  return _buildListView(controller2, false);
+                }
+              })),
+            ),
           ],
         ),
       ),
