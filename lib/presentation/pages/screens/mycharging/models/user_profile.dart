e2386a9bae7c95d91c5edb9321ff8df2a26ad7a5
Aakash Kashyap <aakashkashyap@Aakashs-MacBook-Air.local>
2023-06-23 17:02:28 +0530

    edit profile added

---
+++ lib/presentation/pages/screens/mycharging/models/user_profile.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+
+class UserProfile {
+  UserProfile({
+    required this.name,
+      required this.email,
+      required this.number,
+      required this.image,
+    });
+    
+  final String name;
+  final String email;
+  final String number;
+  final File image;
+}
