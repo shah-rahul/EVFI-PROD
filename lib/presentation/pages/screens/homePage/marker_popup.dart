59b70783dd39e0b8960c7e8373e1c077e2036479
Aakash Kashyap <aakashkashyap0611.com>
2023-07-13 11:51:52 +0530

    modified info window

--- lib/presentation/pages/screens/homePage/marker_popup.dart
+++ lib/presentation/pages/screens/homePage/marker_popup.dart
@@ -1,3 +1,4 @@
+import 'package:EVFI/presentation/resources/color_manager.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 //import 'package:dart_geohash/dart_geohash.dart';
 import 'package:flutter/material.dart';
@@ -9,6 +10,7 @@ import '../../../register/UserChargingRegister.dart';
 import '../../../resources/values_manager.dart';
 
 enum ChargerTypes { A, B, C }
+
 ChargerTypes? selectedType = ChargerTypes.A;
 
 List<String> timings = [
@@ -39,25 +41,32 @@ class _CustomMarkerPopupState extends State<CustomMarkerPopup> {
   bool isBooking = false;
   String str = 'Book Slot';
 
+  void onchanRadio(ChargerTypes val) {
+    setState(() {
+      selectedType = val;
+    });
+  }
+
   Widget callContent() {
     if (isBooking) {
       print('content cancel');
-      return bookingSection(context);
+      return bookingSection(context,onchanRadio);
     } else {
-      print('content book-slot');
+      print('content bookslot');
       return startingSection(context);
     }
   }
 
   void changecontent() {
+    print('hellooooooo');
     isBooking = !isBooking;
-
     if (!isBooking) {
       str = 'Book Slot';
     } else {
       str = 'Back';
     }
     setState(() {});
+    print('hiiií');
     print(isBooking);
   }
 
@@ -66,6 +75,8 @@ class _CustomMarkerPopupState extends State<CustomMarkerPopup> {
     //final width = MediaQuery.of(context).size.width;
     return SingleChildScrollView(
       child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 8),
+        padding: const EdgeInsets.all(3),
         decoration: const BoxDecoration(
             color: Colors.white,
             borderRadius: BorderRadiusDirectional.only(
@@ -86,7 +97,7 @@ class _CustomMarkerPopupState extends State<CustomMarkerPopup> {
             // color: Colors.yellow.withOpacity(0.6),
             // child:
             Padding(
-          padding: const EdgeInsets.all(AppMargin.m12 - 5),
+          padding: const EdgeInsets.all(AppMargin.m12 - 1),
           child:
               Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
             //1st column
@@ -139,6 +150,7 @@ class _CustomMarkerPopupState extends State<CustomMarkerPopup> {
               height: 8,
             ),
             //5th column.........................................................
+            //startingSection(context),
             callContent(),
             //6th column.........................................................
           ]),
@@ -149,116 +161,134 @@ class _CustomMarkerPopupState extends State<CustomMarkerPopup> {
   }
 }
 
-Widget bookingSection(BuildContext context) {
-  return Column(
-    crossAxisAlignment: CrossAxisAlignment.start,
-    children: [
-      const Text('Charger Type',
-          style: TextStyle(fontSize: AppSize.s16, fontWeight: FontWeight.w500)),
-      Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Expanded(
-            child: RadioListTile<ChargerTypes>(
-              contentPadding: const EdgeInsets.all(0.0),
-              value: ChargerTypes.A,
-              title:
-                  const Text('Type A', style: TextStyle(color: Colors.black)),
-              dense: true,
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(5)),
-              activeColor: Colors.amber,
-              groupValue: selectedType,
-              onChanged: (value) {
-                selectedType = value;
-              },
-            ),
-          ),
-          const SizedBox(width: 2),
-          Expanded(
-            child: RadioListTile<ChargerTypes>(
-              contentPadding: const EdgeInsets.all(0.0),
-              value: ChargerTypes.B,
-              title:
-                  const Text('Type B', style: TextStyle(color: Colors.black)),
-              dense: true,
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(5)),
-              activeColor: Colors.amber,
-              groupValue: selectedType,
-              onChanged: (value) {
-                selectedType = value;
-              },
-            ),
-          ),
-          const SizedBox(width: 2),
-          Expanded(
-            child: RadioListTile<ChargerTypes>(
-              contentPadding: const EdgeInsets.all(0.0),
-              value: ChargerTypes.C,
-              title: const Text(
-                'Type C',
-                style: TextStyle(color: Colors.black),
-              ),
-              dense: true,
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(5)),
-              activeColor: Colors.amber,
-              groupValue: selectedType,
-              onChanged: (value) {
-                selectedType = value;
-              },
-            ),
-          ),
-        ],
-      ),
-      DropdownButtonFormField<String>(
-        value: selectedTime,
-        icon: const Icon(Icons.arrow_drop_down_circle, color: Colors.amber),
-        dropdownColor: Colors.amber.shade100,
-        padding: const EdgeInsets.all(8),
+Widget bookingSection(BuildContext context, void Function(ChargerTypes val) onchanRadio) {
+  return Card(
+    shadowColor: ColorManager.CardshadowBottomRight,
+    shape: const RoundedRectangleBorder(borderRadius: BorderRadiusDirectional.all(
+             Radius.circular(15),
+        )),
         elevation: 4,
-        decoration: const InputDecoration(
-          labelStyle: TextStyle(
-              color: Colors.black,
-              fontWeight: FontWeight.w500,
-              fontSize: AppMargin.m14),
-          labelText: "Time Slot",
-          border: UnderlineInputBorder(),
+        color: Colors.white,
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const SizedBox(height: 15),
+        const Row(
+          children: [
+            SizedBox(width: 14),
+            Text('Select your Charger Type',
+                style: TextStyle(fontSize: AppSize.s14, fontWeight: FontWeight.w500)),
+          ],
         ),
-        items: timings.map((String item) {
-          return DropdownMenuItem<String>(
-            value: item,
-            child: Text(
-              item,
-              style: const TextStyle(color: Colors.black),
-            ),
-          );
-        }).toList(),
-        onChanged: (value) {
-          selectedTime = value!;
-        },
-      ),
-      const SizedBox(height: 10),
-      Row(mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Container(
-            height: 35,
-            width: 200,
-            child: ElevatedButton(
-              onPressed: () {},
-              style: const ButtonStyle(
-                  backgroundColor: MaterialStatePropertyAll(Color.fromARGB(255, 88, 222, 92))),
-              child: const Text(
-                'Proceed to Pay',
-                style: TextStyle(
-                    color: Colors.black, fontSize: 12, fontWeight: FontWeight.w700),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Expanded(
+              child: RadioListTile<ChargerTypes>(
+                contentPadding: const EdgeInsets.all(0.0),
+                value: ChargerTypes.A,
+                title:
+                    const Text('Type A', style: TextStyle(color: Colors.black)),
+                dense: true,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(5)),
+                activeColor: Colors.amber,
+                groupValue: selectedType,
+                onChanged: (value) {
+                  onchanRadio(value!);
+                  //selectedType = value;
+                },
               ),
             ),
+            const SizedBox(width: 2),
+            Expanded(
+              child: RadioListTile<ChargerTypes>(
+                contentPadding: const EdgeInsets.all(0.0),
+                value: ChargerTypes.B,
+                title:
+                    const Text('Type B', style: TextStyle(color: Colors.black)),
+                dense: true,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(5)),
+                activeColor: Colors.amber,
+                groupValue: selectedType,
+                onChanged: (value) {
+                  onchanRadio(value!);
+                  //selectedType = value;
+                },
+              ),
+            ),
+            const SizedBox(width: 2),
+            Expanded(
+              child: RadioListTile<ChargerTypes>(
+                contentPadding: const EdgeInsets.all(0.0),
+                value: ChargerTypes.C,
+                title: const Text(
+                  'Type C',
+                  style: TextStyle(color: Colors.black),
+                ),
+                dense: true,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(5)),
+                activeColor: Colors.amber,
+                groupValue: selectedType,
+                onChanged: (value) {
+                  onchanRadio(value!);
+                  //selectedType = value;
+                },
+              ),
+            ),
+          ],
+        ),
+        DropdownButtonFormField<String>(
+          value: selectedTime,
+          icon: const Icon(Icons.arrow_drop_down_circle, color: Colors.amber),
+          dropdownColor: Colors.amber.shade100,
+          padding: const EdgeInsets.all(8),
+          elevation: 4,
+          decoration: const InputDecoration(
+            labelStyle: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.w500,
+                fontSize: AppMargin.m14),
+            labelText: "Time Slot",
+            border: UnderlineInputBorder(),
           ),
-        ],
-      ),
-    ],
+          items: timings.map((String item) {
+            return DropdownMenuItem<String>(
+              value: item,
+              child: Text(
+                item,
+                style: const TextStyle(color: Colors.black),
+              ),
+            );
+          }).toList(),
+          onChanged: (value) {
+            selectedTime = value!;
+          },
+        ),
+        const SizedBox(height: 1),
+        Row(mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              height: 35,
+              width: 200,
+              child: ElevatedButton(
+                onPressed: () {},
+                style: const ButtonStyle(
+                    backgroundColor: MaterialStatePropertyAll(Color.fromARGB(255, 84, 194, 87))),
+                child: const Text(
+                  'Proceed to Pay',
+                  style: TextStyle(
+                      color: Colors.black, fontSize: 12, fontWeight: FontWeight.w700),
+                ),
+              ),
+            ),
+          ],
+        ),
+        const SizedBox(height: 15),
+      ],
+    ),
   );
 }
 
