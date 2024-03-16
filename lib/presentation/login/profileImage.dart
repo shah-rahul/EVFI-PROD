ba623b6d9c815b262d9d96f6f6a6eaacdd02e931
rahul shah <shahrahul795@gmail.com>
2024-03-16 22:21:54 +0530

    small fix

--- lib/presentation/login/profileImage.dart
+++ lib/presentation/login/profileImage.dart
@@ -7,64 +7,69 @@ import '../resources/font_manager.dart';
 import '../resources/strings_manager.dart';
 
 import 'name.dart';
+
 class ProfileImage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
     double screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
-      backgroundColor: ColorManager.appBlack,
-      body: GestureDetector(
-        onTap: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => ProfileUpload()),
-          );
-        },
-        child: Center(
-          child: Stack(
-            children: <Widget>[
-              Positioned(
-                top: screenHeight * 0.08,
-                left: screenWidth * 0.05,
-                child: SizedBox(
-                  height: screenHeight * 0.12,
-                  width: screenWidth * 0.12,
-                  child: SingleChildScrollView(
-                    child: ElevatedButton(
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
-                      style: ElevatedButton.styleFrom(
-                        primary: ColorManager.primary,
-                      ),
-                      child: Row(
-                        children: <Widget>[
-                          Icon(Icons.chevron_left,size: screenWidth * 0.03,color: ColorManager.appBlack,),
-                        ],
+        backgroundColor: ColorManager.appBlack,
+        body: GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => ProfileUpload()),
+            );
+          },
+          child: Center(
+            child: Stack(
+              children: <Widget>[
+                Positioned(
+                  top: screenHeight * 0.08,
+                  left: screenWidth * 0.05,
+                  child: SizedBox(
+                    height: screenHeight * 0.12,
+                    width: screenWidth * 0.12,
+                    child: SingleChildScrollView(
+                      child: ElevatedButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: ColorManager.primary,
+                        ),
+                        child: Row(
+                          children: <Widget>[
+                            Icon(
+                              Icons.chevron_left,
+                              size: screenWidth * 0.03,
+                              color: ColorManager.appBlack,
+                            ),
+                          ],
+                        ),
                       ),
                     ),
                   ),
                 ),
-              ),
-              Positioned(
-                top: screenHeight * 0.18,
-                left: screenWidth * 0.10,
-                child: Image.asset(
-                  ImageAssets.arrow,
-                  width: screenWidth * 0.8,
-                  height: screenHeight * 0.2,
+                Positioned(
+                  top: screenHeight * 0.18,
+                  left: screenWidth * 0.10,
+                  child: Image.asset(
+                    ImageAssets.arrow,
+                    width: screenWidth * 0.8,
+                    height: screenHeight * 0.2,
+                  ),
                 ),
-              ),
-              Positioned(
-                top: screenHeight * 0.28,
-                left: screenWidth * 0.20,
-                child: Image.asset(
-                  ImageAssets.rectangle,
-                  width: screenWidth * 0.6,
-                  height: screenWidth * 0.6,
+                Positioned(
+                  top: screenHeight * 0.28,
+                  left: screenWidth * 0.20,
+                  child: Image.asset(
+                    ImageAssets.rectangle,
+                    width: screenWidth * 0.6,
+                    height: screenWidth * 0.6,
+                  ),
                 ),
-              ),
                 Positioned(
                   top: screenHeight * 0.62,
                   left: screenWidth * 0.15,
@@ -79,67 +84,65 @@ class ProfileImage extends StatelessWidget {
                       shadows: <Shadow>[
                         Shadow(
                             offset: Offset(-4.0, 3.0),
-                            color: ColorManager.dullYellow
-                        ),
+                            color: ColorManager.dullYellow),
                       ],
                     ),
                   ),
                 ),
-              Positioned(
-                top: screenHeight * 0.66,
-                left: screenWidth * 0.15,
-                child: Text(
-                  AppStrings.uploadAnImage,
-                  style: TextStyle(
-                    color: ColorManager.primary,
-                    fontSize: FontSize.s28,
-                    fontFamily: 'fonts/Poppins',
-                    fontWeight: FontWeight.bold,
-                    letterSpacing: 2.0,
-                    shadows: <Shadow>[
-                      Shadow(
-                          offset: Offset(-4.0, 3.0),
-                          color: ColorManager.dullYellow
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              Positioned(
-                top: screenHeight * 0.85,
-                left: screenWidth * 0.10,
-                child: SizedBox(
-                  height: screenHeight * 0.06,
-                  width: screenWidth * 0.8,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => OnBoardingView()),
-                      );
-                    },
-                    style: ElevatedButton.styleFrom(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(screenWidth * 0.02),
-                      ),
-                      primary: ColorManager.primary,
-                    ),
-                    child: Text(
-                      AppStrings.skipForLater,
-                      style: TextStyle(
-                          color: ColorManager.appBlack,
-                          fontFamily: 'Poppins',
-                          fontWeight: FontWeight.bold,
-                          fontSize: FontSize.s20
-                      ),
+                Positioned(
+                  top: screenHeight * 0.66,
+                  left: screenWidth * 0.15,
+                  child: Text(
+                    AppStrings.uploadAnImage,
+                    style: TextStyle(
+                      color: ColorManager.primary,
+                      fontSize: FontSize.s28,
+                      fontFamily: 'fonts/Poppins',
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 2.0,
+                      shadows: <Shadow>[
+                        Shadow(
+                            offset: Offset(-4.0, 3.0),
+                            color: ColorManager.dullYellow),
+                      ],
                     ),
                   ),
                 ),
-              ),
-            ],
+                Positioned(
+                  top: screenHeight * 0.85,
+                  left: screenWidth * 0.10,
+                  child: SizedBox(
+                    height: screenHeight * 0.06,
+                    width: screenWidth * 0.8,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => OnBoardingView()),
+                        );
+                      },
+                      style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius:
+                              BorderRadius.circular(screenWidth * 0.02),
+                        ),
+                        primary: ColorManager.primary,
+                      ),
+                      child: Text(
+                        AppStrings.skipForLater,
+                        style: TextStyle(
+                            color: ColorManager.appBlack,
+                            fontFamily: 'Poppins',
+                            fontWeight: FontWeight.bold,
+                            fontSize: FontSize.s20),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
-        ),
-      )
-    );
+        ));
   }
 }
